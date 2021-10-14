33474fd1cc60f2b8989456ad4392fe36fa11451f
apxxxva784 <apoorva00@outlook.com>
2021-10-14 16:34:19 +0530

    Added Home

--- small_basket/settings.py
+++ small_basket/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('django-insecure-oi3s*ddhm6o)!izj6t&%+jsb*p17w(3n1k@h-hfwho&s=r6*6+')
+SECRET_KEY = 'django-insecure-oi3s*ddhm6o)!izj6t&%+jsb*p17w(3n1k@h-hfwho&s=r6*6+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
