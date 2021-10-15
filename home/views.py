aec32631ad00a432e740421cd73eb6ca7874881f
apxxxva784 <apoorva00@outlook.com>
2021-10-16 02:34:40 +0530

    testing

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, If you see me all works!")
+    return HttpResponse("Hello, If you see me all works I changed!")
