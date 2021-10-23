64c2991dc856204264b4d07261611b649d1920a5
Apoorva <apoorva00@outlook.com>
2021-10-23 08:45:18 +0530

    Added Product and Category models

--- small_basket/settings.py
+++ small_basket/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'small_basket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'small_basket',
+        'USER': 'apoorva',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
