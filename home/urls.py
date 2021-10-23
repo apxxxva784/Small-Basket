e294e2301f5169bf7478ce1b18fee8d3a124e4a1
ayub0809 <ayub0809@gmail.com>
2021-10-23 20:09:33 +0530

    db

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from home import *
 from home import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("products", views.products, name="products"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home/homepage.html')
+
+def products(request):
+    return render(request,'home/products.html')    
