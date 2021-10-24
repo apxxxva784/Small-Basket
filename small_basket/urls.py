1218590b10ad591923936c4410b982a3e1742e34
Apoorva <apoorva00@outlook.com>
2021-10-25 03:03:37 +0530

    Added log in feature

--- small_basket/urls.py
+++ small_basket/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("home.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include("accounts.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
