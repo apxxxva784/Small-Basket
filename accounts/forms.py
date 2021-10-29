de41f7f2c40c7ce8ce19d8523e009d758e81a0e6
Apoorva <apoorva00@outlook.com>
2021-10-30 00:55:47 +0530

    Sign In styles added

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,7 +11,7 @@ class NewUserForm(UserCreationForm):
     last_name = forms.CharField(max_length=20,required=True)
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2", "first_name", "last_name")
+        fields = ("first_name", "last_name","username", "email", "password1", "password2", )
 
 
     def save(self, commit=True):
